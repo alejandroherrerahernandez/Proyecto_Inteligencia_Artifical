2b86679348b838f65bb7dd0e2d900ece62cb0811
alejandroherrerahernandez <alejandroherrera.hernandez@gmail.com>
2016-05-07 19:15:51 -0600

    070516 Se agregó recomendación por similitud

--- src/proyectoia_b/cSistemaDeRecomendacion.java
+++ src/proyectoia_b/cSistemaDeRecomendacion.java
@@ -4,8 +4,12 @@
  * and open the template in the editor.
  */
 package proyectoia_b;
+import java.awt.List;
 import javax.swing.JComboBox;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.function.Consumer;
 
 /**
  *
@@ -37,7 +41,54 @@ public class cSistemaDeRecomendacion {
         BDD.cold_start(cbInteres, sql_interes + user_id + ")");
         BDD.cold_start(cbDescubre, sql_random);
     }
-    public void recomendacion_similitud(){}
+    public void recomendacion_similitud(JComboBox cbSimilitud, String book_id_actual){
+       limpiarCB(cbSimilitud);
+       int z = BDD.get_uniques_id_from_transaction("select count(distinct book_id_origen) from TRANSACTION", "select count(distinct book_id_destino) from TRANSACTION where BOOK_ID_DESTINO not in (select distinct(book_id_origen) from transaction)");
+       if(z > 0){
+           ArrayList<cNodoCombinacion> lista = BDD.listaCombinaciones("select book_id_origen, book_id_destino from transaction");
+           switch(lista.size()){
+               case 1:
+                   BDD.delete("delete from similares");
+                   BDD.insert("insert into similares (book_id_o, book_id_d) values ('" + lista.get(0).origen + "','" + lista.get(0).destino + "')");
+                   break;
+               default:
+                   for(int i = 0; i <lista.size(); i++){
+                       if(!lista.get(i).visto){
+                           lista.get(i).aumentar_ocurrencias();
+                           lista.get(i).set_visto();
+                           if(i != lista.size() -1){
+                               for(int j = i +1; j <lista.size(); j++){
+                                   if(!lista.get(j).visto && nodos_iguales(lista.get(i), lista.get(j))){
+                                       lista.get(i).aumentar_ocurrencias();
+                                       lista.get(j).set_visto();
+                                    }
+                                }
+                            } 
+                        }
+                    }
+                   BDD.delete("delete from similares");
+                   ingresar_combinaciones_satisfactorias(z, lista);
+                   break;
+           }
+       }
+       
+    }
+    void ingresar_combinaciones_satisfactorias(int z, ArrayList<cNodoCombinacion> lista){
+       z = z / 2;
+       for(int i = 0; i < lista.size(); i++){
+           if(lista.get(i).ocurrencias >= z){
+               BDD.insert("insert into similares (book_id_o, book_id_d) values ('" + lista.get(i).origen + "','" + lista.get(i).destino + "')");
+           }
+       }
+    }
+    boolean nodos_iguales(cNodoCombinacion a, cNodoCombinacion b){
+        if(a.origen.equals(b.origen))
+            return a.destino.equals(b.destino);
+        else
+            if(a.destino.equals(b.origen))
+                return a.origen.equals(b.destino);
+        return false;
+    }
     public void recomendacion_colaborativa(){}
     void limpiarCB(JComboBox cb){
         cb.removeAllItems();
